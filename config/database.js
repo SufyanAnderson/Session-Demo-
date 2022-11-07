3817ee2c7ee3fcd9fc15b6e317bd0495abdf4759
yusha7 <sufyananderson@gmail.com>
2022-11-06 22:38:30 -0500

    project

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sufyanA21:demo@cluster0.h7vdga4.mongodb.net/demoday?retryWrites=true&w=majority', 
+    'dbName': 'demoday'
+};
